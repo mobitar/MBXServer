0eb2f33201e03c59ad55276fb4355a4c5fab6ab9
Mo Bitar <me@bitar.io>
2015-03-27 17:03:00 -0500

    updates

--- MBXJSONResponseSerializerWithData.m
+++ MBXJSONResponseSerializerWithData.m
@@ -10,22 +10,22 @@
 
 @implementation MBXJSONResponseSerializerWithData
 
-- (id)responseObjectForResponse:(NSURLResponse *)response
-                           data:(NSData *)data
-                          error:(NSError *__autoreleasing *)error
-{
-    if (![self validateResponse:(NSHTTPURLResponse *)response data:data error:error]) {
-        if (*error != nil) {
-            NSMutableDictionary *userInfo = [(*error).userInfo mutableCopy];
-            userInfo[MBXJSONResponseSerializerWithDataKey] = data;
-            NSError *newError = [NSError errorWithDomain:(*error).domain code:(*error).code userInfo:userInfo];
-            (*error) = newError;
-        }
-        
-        return (nil);
-    }
-    
-    return ([super responseObjectForResponse:response data:data error:error]);
-}
+//- (id)responseObjectForResponse:(NSURLResponse *)response
+//                           data:(NSData *)data
+//                          error:(NSError *__autoreleasing *)error
+//{
+//    if (![self validateResponse:(NSHTTPURLResponse *)response data:data error:error]) {
+//        if (*error != nil) {
+//            NSMutableDictionary *userInfo = [(*error).userInfo mutableCopy];
+//            userInfo[MBXJSONResponseSerializerWithDataKey] = data;
+//            NSError *newError = [NSError errorWithDomain:(*error).domain code:(*error).code userInfo:userInfo];
+//            (*error) = newError;
+//        }
+//        
+//        return (nil);
+//    }
+//    
+//    return [super responseObjectForResponse:response data:data error:error];
+//}
 
 @end
